a2f130718ca7be56968b1734821f8c273395f410
JonathanHavard <167351822+JonathanHavard@users.noreply.github.com>
2024-04-19 13:25:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,27 @@ using namespace std;
 #include "ItemToPurchase.h"
 
 int main() {
-   
-   /* Type your code here */
+   ItemPurchase-> item1, item2;
+   for (int i=0; i<2; i++){
+   if (i =0){
+   cout << "item " << i << endl;
+   cout << "Enter the item name: " << endl;
+   cin >> item1.name;
+   cout << "Enter the item price: "<< endl;
+   cin >> item1.price;
+   cout << "Enter the item quantity: " << endl;
+   cin >> item1.quantity;
+   }
+   if (i =1){
+   cout << "item " << i << endl;
+   cout << "Enter the item name: " << endl;
+   cin >> item2.name;
+   cout << "Enter the item price: "<< endl;
+   cin >> item2.price;
+   cout << "Enter the item quantity: " << endl;
+   cin >> item2.quantity;
+   }
+   }
    
    return 0;
 }
